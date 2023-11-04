c1ca2a770cb3332dbb90cfb75280ead8bd59291c
Edward Nguyen <ed@delimister.com>
2023-11-05 02:27:37 +0700

    :sparkles: experimental custom ytb client

--- src/pages/api/get-playable-url.ts
+++ src/pages/api/get-playable-url.ts
@@ -3,16 +3,15 @@ import {NextApiRequest, NextApiResponse} from "next";
 
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  const youtube = await Innertube.create({
-    location: 'VN'
-  });
+  const youtube = await Innertube.create();
   let data = [];
   for (let id of req.body.ids || []) {
-    const info = await youtube.getBasicInfo(id);
+    const info = await youtube.getInfo(id, 'ANDROID');
     const format = info.chooseFormat({type: 'audio', quality: 'best'});
     const url = format?.decipher(youtube.session.player);
     data.push({
       id, url,
+      metadata: info,
     });
   }
   res.json({
