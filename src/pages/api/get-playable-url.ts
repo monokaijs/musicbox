e26598bc5e3564d6bfaaccc101a868662613e245
Edward Nguyen <nhanna@northstudio.vn>
2024-07-13 14:21:42 +0700

    :bug: update deps & remove distube

--- src/pages/api/get-playable-url.ts
+++ src/pages/api/get-playable-url.ts
@@ -1,25 +1,18 @@
 import {Innertube} from 'youtubei.js';
 import {NextApiRequest, NextApiResponse} from "next";
-import ytdl from "@distube/ytdl-core";
 
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const youtube = await Innertube.create();
   let data = [];
   for (let id of req.body.ids || []) {
-    const info = await ytdl.getInfo('https://www.youtube.com/watch?v=' + id);
-    if (info && info.formats) {
-      const formats = info.formats;
-      const audioFormats = info.formats.filter(x => x.mimeType?.startsWith('audio/')).sort((a, b) => {
-        return (b?.audioBitrate || 0) - (a?.audioBitrate || 0);
-      });
-      if (audioFormats.length > 0) {
-        data.push({
-          id,
-          url: audioFormats[0].url,
-          metadata: info,
-        })
-      }
-    }
+    const info = await youtube.getBasicInfo(id);
+    const format = info.chooseFormat({ type: 'audio', quality: 'best' });
+    data.push({
+      id,
+      url: format?.decipher(youtube.session.player),
+      metadata: info,
+    })
   }
   res.json({
     data,
